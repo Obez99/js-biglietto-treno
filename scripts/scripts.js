eb40021b4939cdfdaefb71fe41680e1e49790509
Obez99 <giuliogx@live.it>
2021-10-08 14:19:38 +0200

    Adds prompts for age and kilometers

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+alert("Benvenuto! Questo script calcolerà il prezzo totale del tuo viaggio, verranno applicati degli sconti in base all'età.");
+
+const kilometers = parseInt(prompt("Per prima cosa, quanti chilometri vorresti percorrere?"));
+const age = parseInt(prompt(`${kilometers} chilometri? D'accordo, quanti anni hai?`));
