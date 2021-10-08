5fef0d36a4cfc64eef1179f638d331cd19ff0e8b
Obez99 <giuliogx@live.it>
2021-10-08 14:57:50 +0200

    Calculates discount when underage or over 65

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,5 +4,17 @@ const kilometers = parseInt(prompt("Per prima cosa, quanti chilometri vorresti p
 const age = parseInt(prompt(`${kilometers} chilometri? D'accordo, quanti anni hai?`));
 
 const ticketPrice = (kilometers * 0.21).toFixed(2);
+const under18 = (ticketPrice * 20 / 100).toFixed(2);
+const over65 = (ticketPrice * 40 / 100).toFixed(2);
+
 
 alert("Il prezzo totale del biglietto è di: €" + ticketPrice);
+
+if(age < 18){
+  alert("Dato che sei minorenne, hai diritto a uno sconto del 20% sul tuo biglietto, pari a €" + under18);
+  alert("Il prezzo finale del tuo biglietto è di €" + (ticketPrice - under18).toFixed(2));
+}
+else if(age > 65){
+  alert("Dato che sei un over 65, hai diritto a uno sconto del 40% sul tuo biglietto, pari a €" + over65);
+  alert("Il prezzo finale del tuo biglietto è di €" + (ticketPrice - over65).toFixed(2));
+}
